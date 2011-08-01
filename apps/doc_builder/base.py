f103017b830880ae9d0a6be87d0d76bf938e7dfe
Corey Oordt <coordt@washingtontimes.com>
2011-08-01 18:04:46 -0400

    Editing the doc_builder class

--- apps/doc_builder/base.py
+++ apps/doc_builder/base.py
@@ -92,18 +92,17 @@ class BaseBuilder(object):
         the failed command.
         """
         environment = os.environ.copy()
-        environment['READTHEDOCS'] = 'True'
         cwd = os.getcwd()
         if not commands:
             raise ValueError("run() requires one or more command-line strings")
-
+        
         for command in commands:
             print("Running: '%s'" % command)
             try:
                 p = subprocess.Popen(command.split(), shell=False, cwd=cwd,
                                      stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                                      env=environment)
-
+                
                 out, err = p.communicate()
                 ret = p.returncode
             except:
@@ -111,5 +110,5 @@ class BaseBuilder(object):
                 err = traceback.format_exc()
                 ret = -1
                 print "Command failed: %s" % err
-
+        
         return (ret, out, err)
